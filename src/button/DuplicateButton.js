0667b0f1b3b68523622b448293ab8c1137b0e63e
teppei <teppei0830.4869@gmail.com>
2023-11-04 23:53:39 +0900

    絞り込みなしの文字にも装飾を加えました。

--- src/button/DuplicateButton.js
+++ src/button/DuplicateButton.js
@@ -60,13 +60,17 @@ const ParentComponent = ({ onChangeAllValue }) => {
       <div>
         <span>以下の人物で AND 検索しました</span>
         <div className="py-4">
-          {allInputValues.length === 0 || allInputValues.includes(undefined)
-            ? "絞り込みなし"
-            : allInputValues.map((component) => (
-                <span className="bg-emerald-100 rounded-3xl my-4 mx-2 p-2 opacity-70">
-                  {component}
-                </span>
-              ))}
+          {allInputValues.length === 0 || allInputValues.includes(undefined) ? (
+            <span className="bg-emerald-100 rounded-3xl my-4 mx-2 p-2 opacity-70">
+              絞り込みなし
+            </span>
+          ) : (
+            allInputValues.map((component) => (
+              <span className="bg-emerald-100 rounded-3xl my-4 mx-2 p-2 opacity-70">
+                {component}
+              </span>
+            ))
+          )}
         </div>
       </div>
     </div>
