e4782cc81a56765b4f14455c839075b594a64016
teppei <teppei0830.4869@gmail.com>
2023-11-08 22:20:25 +0900

    リンクを別タブで開くかどうかを引数で指定できるようにしました。

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,18 +4,24 @@ import LinkButton from "./LinkButton";
 const Footer: React.FC = () => {
   return (
     <div className="bg-emerald-100 p-4 text-xl flex gap-x-4 overflow-auto whitespace-nowrap">
-      <LinkButton to="https://github.com/ASAP-BYAF" plusStyle="bg-emerald-500">
+      <LinkButton
+        to="https://github.com/ASAP-BYAF"
+        plusStyle="bg-emerald-500"
+        target="_blank"
+      >
         github
       </LinkButton>
       <LinkButton
         to="https://comp.chem.tohoku.ac.jp/ComplexRI/"
         plusStyle="bg-emerald-500"
+        target="_blank"
       >
         ComplexRI
       </LinkButton>
       <LinkButton
         to="https://snippet.hopto.org/snippet/snippet/"
         plusStyle="bg-emerald-500"
+        target="_blank"
       >
         SnippetApp
       </LinkButton>
--- src/LinkButton.tsx
+++ src/LinkButton.tsx
@@ -5,15 +5,17 @@ type LinkButtonProps = {
   to: string;
   children: ReactNode;
   plusStyle?: string;
+  target?: string;
 };
 
 const LinkButton: React.FC<LinkButtonProps> = (props: LinkButtonProps) => {
-  const { to, children, plusStyle = "???" } = props;
+  const { to, children, plusStyle = "???", target = "" } = props;
   return (
     <Link
       className={`px-4 py-2 rounded-md font-bold ${plusStyle}`}
       to={to}
-      target="_blank"
+      // target="_blank"
+      target={target}
     >
       {children}
     </Link>
