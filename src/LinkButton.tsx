0827db68c4b4d7d7c338065b5c90189017cece89
teppei <teppei0830.4869@gmail.com>
2023-11-16 21:51:13 +0900

    Header のデザインを修正しています。
    アプリ名とロゴを入れました。src/header にまとめています。

--- src/LinkButton.tsx
+++ src/LinkButton.tsx
@@ -12,7 +12,7 @@ const LinkButton: React.FC<LinkButtonProps> = (props: LinkButtonProps) => {
   const { to, children, plusStyle = "???", target = "" } = props;
   return (
     <Link
-      className={`px-4 py-2 rounded-md font-bold ${plusStyle}`}
+      className={`px-4 py-2 font-bold ${plusStyle}`}
       to={to}
       // target="_blank"
       target={target}
