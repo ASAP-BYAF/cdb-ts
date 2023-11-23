7c3a23de39e9c98bf1be4ccc98fb950378875d20
teppei <teppei0830.4869@gmail.com>
2023-11-23 21:11:58 +0900

    サインアウトのエンドポイントをたたく関数を作成しました

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,10 +1,11 @@
 import { fetcher } from "./fetcher.js";
 
 type SigninRetProps = { message: string } | number;
+type SignoutRetProps = { message: string } | number;
 type ConfirmRetProps = { message: string } | number;
 
 export const signin = async (pass: string): Promise<SigninRetProps> => {
-  const url = `http://localhost:8000/auth/signin`;
+  const url = `${process.env.REACT_APP_AUTH_API_HOST}/signin`;
   const data = {
     method: "POST",
     body: JSON.stringify({
@@ -20,7 +21,21 @@ export const signin = async (pass: string): Promise<SigninRetProps> => {
 };
 
 export const confirm = async (): Promise<ConfirmRetProps> => {
-  const url = `http://localhost:8000/auth/confirm`;
+  const url = `${process.env.REACT_APP_AUTH_API_HOST}/confirm`;
+  const data = {
+    method: "POST",
+    body: JSON.stringify({}),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    credentials: "include",
+  };
+  const res = await fetcher(url, data);
+  return res;
+};
+
+export const signout = async (): Promise<SignoutRetProps> => {
+  const url = `${process.env.REACT_APP_AUTH_API_HOST}/signout`;
   const data = {
     method: "POST",
     body: JSON.stringify({}),
