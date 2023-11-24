cd8cc4bcb7c4a57ab3de677474acafc747661211
teppei <teppei0830.4869@gmail.com>
2023-11-24 21:54:15 +0900

    新たに ToggleStatusButton.tsx を作成しました。
    ToSigninButton.tsx と ToSignoutButton.tsx の機能が似ていたので
    一つにまとめました。

---
+++ src/header/ToggleStatusButton.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LinkButton from "LinkButton";
+
+export type ToggleStatusButtonProps = { link: string; label: string };
+
+const ToggleStatusButton: React.FC<ToggleStatusButtonProps> = (props) => {
+  const { link, label } = props;
+  return (
+    <LinkButton to={link} plusStyle="hover:bg-emerald-100 outline outline-1">
+      {label}
+    </LinkButton>
+  );
+};
+
+export default ToggleStatusButton;
