949b5e7def25eedea13c45b472b00f0f87e8c842
teppei <teppei0830.4869@gmail.com>
2023-11-25 20:43:21 +0900

    Home.js を ts に書き換えました。

---
+++ src/home/Home.tsx
@@ -0,0 +1,34 @@
+import BaseFrame from "BaseFrame";
+import WiseWord from "home/WiseWord";
+
+const Home = (): JSX.Element => {
+  return (
+    <BaseFrame>
+      <>
+        <WiseWord person="江戸川コナン" word="真実はいつも一つ" />
+        <WiseWord
+          person="ホームズ"
+          word="人生という無色の糸の束には殺人という真っ赤な糸が混ざっている。それを解きほぐすことが我々の仕事なんじゃないのかな。"
+          reference="ベイカー街の亡霊"
+        />
+        <WiseWord
+          person="ベルモット"
+          word="secret makes a woman woman"
+          reference="42 巻, File9"
+        />
+        <WiseWord
+          person="江戸川コナン"
+          word="不可能な物を除外していって残った物が… たとえどんなに信じられなくても… それが真相なんだ!!"
+          reference="28 巻, File9"
+        />
+        <WiseWord
+          person="灰原哀"
+          word="相手はイルカ…そう…海の人気者…暗く冷たい海の底から逃げてきたサメなんかじゃとても歯が立たないでしょうね…"
+          reference="31 巻, File5"
+        />
+      </>
+    </BaseFrame>
+  );
+};
+
+export default Home;
