6deda2e389ccc915502e979ef998ef377db21230
teppei <teppei0830.4869@gmail.com>
2023-11-25 20:26:07 +0900

    rename.js を ts に書き換えました。

--- src/util/rename.ts
+++ src/util/rename.ts
@@ -1,11 +1,12 @@
+import { MyObj } from "./util";
 import { deleteItemFromArrayInObject, deleteItemFromObject } from "./delete";
 
 // 配列の特定の値を別の値に変更する。
 //  例: [value1, value2, value3, ...]
 //        `--> [value1_prime, value2, value3, ...]
-export const renameItemInArray = (array, x, x_prime) => {
+export const renameItemInArray = (array: MyObj, x: any, x_prime: any) => {
   const x_idx_in_items = array.indexOf(x);
-  return array.map((item, index) =>
+  return array.map((item: any, index: number) =>
     index === x_idx_in_items ? x_prime : item
   );
 };
@@ -13,7 +14,7 @@ export const renameItemInArray = (array, x, x_prime) => {
 // オブジェクトの特定のキーを変更する。
 // 例: { key1: value1, key2: value2, ...}
 //    `--> { key1_prime: value1, key2: value2, ...}
-export const renameKeyInObject = (obj, x, x_prime) => {
+export const renameKeyInObject = (obj: MyObj, x: any, x_prime: any) => {
   const value = obj[x];
   const obj_ = deleteItemFromObject(obj, x);
   obj_[x_prime] = value;
@@ -21,7 +22,12 @@ export const renameKeyInObject = (obj, x, x_prime) => {
 };
 
 // { key1: [array1], key2: [array2], ...} というオブジェクトの配列のある値を別の値に変更する。
-export const renameItemInArrayInObject = (obj, key, x, x_prime) => {
+export const renameItemInArrayInObject = (
+  obj: MyObj,
+  key: any,
+  x: any,
+  x_prime: any
+) => {
   const obj_ = deleteItemFromArrayInObject(obj, key, x);
   obj_[key] = [...obj_[key], x_prime];
   return obj_;
