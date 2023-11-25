7348ea8917ed16f5757bfe9efe59473aa2396190
teppei <teppei0830.4869@gmail.com>
2023-11-25 16:44:49 +0900

    新たに ToggleStatusButtonGroup.tsx を作成しました。
    HeaderTitle.tsx 内の３つの toggleStatusGroup をまとめました。

--- src/header/HeaderTitle.tsx
+++ src/header/HeaderTitle.tsx
@@ -1,30 +1,8 @@
-import { useState } from "react";
 import React from "react";
 import { Link } from "react-router-dom";
-// import ToSigninButton from "./ToSigninButton";
-// import ToSignoutButton from "./ToSignoutButton";
-import ToggleStatusButton from "./ToggleStatusButton";
-import { confirm } from "api/auth";
+import ToggleStatusButtonGroup from "./ToggleStatusButtonGroup";
 
 const HeaderTitle: React.FC = () => {
-  const [signinFlag, setSigninFlag]: [
-    string,
-    React.Dispatch<React.SetStateAction<string>>
-  ] = useState("");
-
-  const confirmSignin = async () => {
-    const res = await confirm();
-
-    // サインインが確認できなかった時はサインイン画面にリダイレクト
-    if (res === 401) {
-      setSigninFlag("out");
-    } else {
-      setSigninFlag("in");
-    }
-  };
-
-  confirmSignin();
-
   return (
     <div className="bg-slate-100">
       <div className="p-10 md:flex">
@@ -44,15 +22,7 @@ const HeaderTitle: React.FC = () => {
           </div>
         </div>
         <div className="flex justify-center md:flex-1 md:justify-end">
-          {signinFlag === "" ? (
-            <ToggleStatusButton link="/" label="Now Loading..." />
-          ) : undefined}
-          {signinFlag === "in" ? (
-            <ToggleStatusButton link="/signout" label="サインアウト" />
-          ) : undefined}
-          {signinFlag === "out" ? (
-            <ToggleStatusButton link="/signin" label="サインイン" />
-          ) : undefined}
+          <ToggleStatusButtonGroup />
         </div>
       </div>
     </div>
---
+++ src/header/ToggleStatusButtonGroup.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import ToggleStatusButton from "./ToggleStatusButton";
+import { confirm } from "api/auth";
+
+const ToggleStatusButtonGroup = (): JSX.Element => {
+  type Status = "" | "in" | "out";
+  const [signinFlag, setSigninFlag]: [
+    Status,
+    React.Dispatch<React.SetStateAction<Status>>
+  ] = useState<Status>("");
+
+  const confirmSignin = async () => {
+    const res = await confirm();
+
+    // サインインが確認できなかった時はサインイン画面にリダイレクト
+    if (res === 401) {
+      setSigninFlag("out");
+    } else {
+      setSigninFlag("in");
+    }
+  };
+
+  confirmSignin();
+
+  console.log(signinFlag);
+  return (
+    <>
+      {signinFlag === "" ? (
+        <ToggleStatusButton link="/" label="Now Loading..." />
+      ) : undefined}
+      {signinFlag === "in" ? (
+        <ToggleStatusButton link="/signout" label="サインアウト" />
+      ) : undefined}
+      {signinFlag === "out" ? (
+        <ToggleStatusButton link="/signin" label="サインイン" />
+      ) : undefined}
+    </>
+  );
+};
+
+export default ToggleStatusButtonGroup;
