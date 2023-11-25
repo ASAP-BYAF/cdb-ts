7c99f52b2598b9ac3301c6464ab24812e26d0e5d
teppei <teppei0830.4869@gmail.com>
2023-11-25 17:59:25 +0900

    search.js を ts に書き換えました。

---
+++ src/api/search.ts
@@ -0,0 +1,23 @@
+import { fetcher } from "./fetcher";
+
+export const getAll = async () => {
+  const url = `${process.env.REACT_APP_DB_API_HOST}/search`;
+  const data = { method: "GET" };
+  const res = await fetcher(url, data);
+  return res;
+};
+
+export const getFilteredByTask = async (filterList: (string | undefined)[]) => {
+  const url = `${process.env.REACT_APP_DB_API_HOST}/search_filtered_by_task`;
+  const data = {
+    method: "POST",
+    body: JSON.stringify({
+      task_list: filterList,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  };
+  const res = await fetcher(url, data);
+  return res;
+};
