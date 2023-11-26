00a228006f75075f3c971ae0c99de7875f2579eb
teppei <teppei0830.4869@gmail.com>
2023-11-26 17:13:54 +0900

    select 文を使ってより簡潔に書き直しました。

--- src/app/register/RadioButtonForm.js
+++ src/app/register/RadioButtonForm.js
@@ -18,24 +18,32 @@ function RadioButtonForm(props) {
 
   const [selectedOptions, setSelectedOptions] = useState({});
 
-  // 選択状況を得たときはそれを各選択制の初期値に設定します。
+  // 最初のロード時に前回までの選択状況を取得し、初期値にする。
   useMemo(() => {
     setSelectedOptions(selectedOptionsBefore);
   }, [selectedOptionsBefore]);
 
+  // 人物名の追加、削除、変更に応じて選択状況を変化させる。
   useMemo(() => {
-    const sign = questionsDiff[0];
-    const diff = questionsDiff[1];
-    if (sign === "added") {
-      setSelectedOptions((prev) =>
-        concatObject(prev, arrayToObject(diff, NaN))
-      );
-    } else if (sign === "deleted") {
-      setSelectedOptions((prev) => deleteItemFromObject(prev, diff));
-    } else if (sign === "renamed") {
-      const oldValue = diff[0];
-      const newValue = diff[1];
-      setSelectedOptions((prev) => renameKeyInObject(prev, oldValue, newValue));
+    const [sign, diff] = questionsDiff;
+
+    switch (sign) {
+      case "added":
+        setSelectedOptions((prev) =>
+          concatObject(prev, arrayToObject(diff, NaN))
+        );
+        break;
+      case "deleted":
+        setSelectedOptions((prev) => deleteItemFromObject(prev, diff));
+        break;
+      case "renamed":
+        const [oldValue, newValue] = diff;
+        setSelectedOptions((prev) =>
+          renameKeyInObject(prev, oldValue, newValue)
+        );
+        break;
+      default:
+        break;
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [questionsDiff]);
@@ -62,46 +70,49 @@ function RadioButtonForm(props) {
     provideOptionChange({ name: name, value: NaN });
   };
 
+  const handleButtonClick = (icon) => {
+    switch (icon) {
+      case "✕":
+        return handleDeleteClick;
+      case "✑":
+        return handleRenameClick;
+      case "↻":
+        return handleResetClick;
+      default:
+        return () => {};
+    }
+  };
+
   const memoQuestions = useMemo(() => {
-    const tmpOptionList = [];
-    tmpOptionList.push(
-      options.map((option) => <span key={option}>{option}</span>)
-    );
-    questions.forEach((elem, idx) => {
-      tmpOptionList.push(
-        <div key={elem} className="py-1">
-          <RadioButton
-            label={elem}
-            options={options}
-            selectedValue={selectedOptions[elem]}
-            onChange={handleOptionChange}
-          />
+    return questions.flatMap((elem) => (
+      <div key={elem} className="py-2">
+        <RadioButton
+          label={elem}
+          options={options}
+          selectedValue={selectedOptions[elem]}
+          onChange={handleOptionChange}
+        />
+        {["✕", "✑", "↻"].map((icon, idx) => (
           <G2WButton
-            label="✕"
+            key={idx}
+            label={icon}
             name={elem}
-            onclick={handleDeleteClick}
+            onclick={handleButtonClick(icon)}
             plusStyle="mx-1 px-1 text-white hover:text-emerald-500"
           />
-          <G2WButton
-            label="✑"
-            name={elem}
-            onclick={handleRenameClick}
-            plusStyle="mx-1 px-1 text-white hover:text-emerald-500"
-          />
-          <G2WButton
-            label="↻"
-            name={elem}
-            onclick={handleResetClick}
-            plusStyle="mx-1 px-1 text-white hover:text-emerald-500"
-          />
-        </div>
-      );
-    });
-    return tmpOptionList;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        ))}
+      </div>
+    ));
   }, [questions, selectedOptions, options]);
 
-  return <div>{memoQuestions}</div>;
+  return (
+    <div>
+      {options.map((option) => (
+        <span key={option}>{option}</span>
+      ))}
+      {memoQuestions}
+    </div>
+  );
 }
 
 export default RadioButtonForm;
