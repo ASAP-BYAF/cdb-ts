7b2350f67f031e6ddf92ba96e64ac457a39991c1
teppei <teppei0830.4869@gmail.com>
2023-11-29 21:53:47 +0900

    deleteItemFromObj の引数の key の型を単一の値に限定しました。

--- src/util/delete.ts
+++ src/util/delete.ts
@@ -8,14 +8,18 @@ export const deleteItemFromArray = (array: MyArr, x: any) => {
 };
 
 // Rf: https://chaika.hatenablog.com/entry/2020/05/13/083000
-// { key1: value1, key2: value2, ...} というオブジェクトからある値を削除する。
-export const deleteItemFromObject = (prev: MyObj, key: any) => {
+// { key1: value1, key2: value2, ...} というオブジェクトからキーを指定してある値を削除する。
+export const deleteItemFromObject = (prev: MyObj, key: string | number) => {
   const { [key]: _, ...newObj } = prev;
   return newObj;
 };
 
-// { key1: [array1], key2: [array2], ...} というオブジェクトの配列からある値を削除する。
-export const deleteItemFromArrayInObject = (obj: MyObj, key: any, x: any) => {
+// { key1: [array1], key2: [array2], ...} というオブジェクトのあるキーの配列からある値を削除する。
+export const deleteItemFromArrayInObject = (
+  obj: MyObj,
+  key: string | number,
+  x: any
+) => {
   obj[key] = deleteItemFromArray(obj[key], x);
   return obj;
 };
