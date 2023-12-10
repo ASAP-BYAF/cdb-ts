5877c6b759faf5073a04b767d14a687322f34afc
teppei <teppei0830.4869@gmail.com>
2023-12-10 12:17:01 +0900

    MultiTextAreaWithButton.tsx を修正しました。
    デフォルト値を配列で受け取り、各デフォルト値が入ったテキストエリア
    作成できるようにしました。

--- src/components/form/MultiTextAreaWithButton.tsx
+++ src/components/form/MultiTextAreaWithButton.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Trans2GButton from "components/button/Trans2GButton";
 import TextAreaWithButton from "components/form/TextAreaWithButton";
 
@@ -10,7 +10,7 @@ type MultiTextAreaWithButtonProps = {
   plusStyleParent?: string;
   plusStyleTextArea?: string;
   placeholder?: string;
-  defaultValue?: string;
+  defaultValueList?: string[];
   handleOnChangeAdditional?: StringToVoidFunction;
   plusStyleButtonForTextArea?: string[];
   buttonLabelForTextArea?: string[];
@@ -26,7 +26,7 @@ const MultiTextAreaWithButton = (
     plusStyleParent = "",
     plusStyleTextArea = "",
     placeholder = "ここに入力してください。",
-    defaultValue = "",
+    defaultValueList = [],
     handleOnChangeAdditional = (x) => {},
     plusStyleButtonForTextArea = [""],
     buttonLabelForTextArea = ["削除"],
@@ -37,14 +37,33 @@ const MultiTextAreaWithButton = (
   );
   const [serialNum, setSerialNum] = useState(0);
 
-  const handleAddItem = () => {
-    setItems((prevItems) => [
-      ...prevItems,
+  useEffect(() => {
+    // デフォルト値が更新されたらテキストエリアの状態を初期化
+    let serialNumber = 0;
+    let textAreaList = [] as { id: number; element: JSX.Element }[];
+
+    defaultValueList.forEach((defaultValue) => {
+      const res = handleAddItem(defaultValue, serialNumber, textAreaList);
+      serialNumber = res[0];
+      textAreaList = res[1];
+    });
+  }, [defaultValueList]);
+
+  const handleAddItem = (
+    defaultValue: string = "",
+    serialNumber: number = serialNum,
+    textAreaList: { id: number; element: JSX.Element }[] = items
+  ): [number, { id: number; element: JSX.Element }[]] => {
+    // forEach 内で繰り返し呼ぶときにのみ更新前の状態をあらわに与えます。
+    // これは forEach 内では状態の更新が反映されないため、別の変数で
+    // 状態の管理をしているからです。
+    const newItems = [
+      ...textAreaList,
       {
-        id: serialNum,
+        id: serialNumber,
         element: (
           <TextAreaWithButton
-            key={serialNum}
+            key={serialNumber}
             buttonLabel={buttonLabelForTextArea}
             plusStyleParent={plusStyleParent}
             plusStyleTextArea={plusStyleTextArea}
@@ -54,7 +73,7 @@ const MultiTextAreaWithButton = (
             defaultValue={defaultValue}
             handleClick={[
               (inputText) => {
-                handleDeleteItem(serialNum);
+                handleDeleteItem(serialNumber);
                 handleClickAdditional[0](inputText);
               },
               ...handleClickAdditional.slice(1),
@@ -62,8 +81,13 @@ const MultiTextAreaWithButton = (
           />
         ),
       },
-    ]);
-    setSerialNum((prevId) => prevId + 1);
+    ];
+    const newId = serialNumber + 1;
+
+    // 状態を更新
+    setItems(newItems);
+    setSerialNum(newId);
+    return [newId, newItems];
   };
 
   const handleDeleteItem = (itemId: number) => {
@@ -74,7 +98,7 @@ const MultiTextAreaWithButton = (
     <>
       <Trans2GButton
         label={addButtonLabel}
-        onclick={handleAddItem}
+        onclick={() => handleAddItem()}
         plusStyle={`mt-4 mb-2 py-2 px-4 ${plusStyleAddButton}`}
       />
       <div className="flex justify-center flex-wrap">
