98d2a2e457f42ec5cd7fe495ffff5d00b104a7ba
teppei <teppei0830.4869@gmail.com>
2024-01-02 15:55:29 +0900

    HeaderNav.tsx を編集しました。
    ナビゲーションにホバーしたときのスタイル
    を LinkButton のアクティブ時に合わせました。

--- src/components/header/HeaderNav.tsx
+++ src/components/header/HeaderNav.tsx
@@ -5,19 +5,19 @@ const HeaderNav = (): JSX.Element => {
     <div className="bg-emerald-500 text-xl flex sticky top-0 z-20">
       <LinkButton
         to="/search"
-        plusStyle="hover:bg-emerald-100 flex-1 outline outline-1"
+        plusStyle="hover:bg-[#9C27B0] hover:text-white hover:font-bold flex-1 outline outline-1"
       >
         登場シーン
       </LinkButton>
       <LinkButton
         to="/wiseword"
-        plusStyle="hover:bg-emerald-100 flex-1 outline outline-1"
+        plusStyle="hover:bg-[#9C27B0] hover:text-white hover:font-bold flex-1 outline outline-1"
       >
         名言
       </LinkButton>
       <LinkButton
         to="/"
-        plusStyle="hover:bg-emerald-100 flex-1 outline outline-1"
+        plusStyle="hover:bg-[#9C27B0] hover:text-white hover:font-bold flex-1 outline outline-1"
       >
         事件
       </LinkButton>
