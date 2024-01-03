4ba9dfa4ec509dd7e6be1b63dd3287c41db4c007
teppei <teppei0830.4869@gmail.com>
2024-01-03 22:09:42 +0900

    RefineList.tsx を編集しました。
    共通要素を subcontent としてまとめました。

--- src/app/search/RefineList.tsx
+++ src/app/search/RefineList.tsx
@@ -3,38 +3,37 @@ const SearchCondiList = (props: { charaList: (string | undefined)[] }) => {
 
   let content: JSX.Element = <></>;
   if (charaList.length === 0 || charaList.includes(undefined)) {
-    content = <span className="text-white text-2xl">全話を表示しています</span>;
-  } else if (charaList.length === 1) {
-    content = (
-      <>
-        <span className="text-white text-2xl">以下の人物で検索しました</span>
-        <div className="overflow-x-auto whitespace-nowrap">
-          {charaList.map((component) => (
-            <span className="bg-amber-300 text-black inline-block rounded-3xl my-4 mx-2 p-2">
-              {component}
-            </span>
-          ))}
-        </div>
-      </>
-    );
+    content = <span className="text-white text-lg">全話を表示しています</span>;
   } else {
-    content = (
-      <>
-        <span className="text-white text-2xl">
-          以下の人物で AND 検索しました
-        </span>
-        <div className="overflow-x-auto whitespace-nowrap">
-          {charaList.map((component) => (
-            <span className="bg-amber-300 text-black inline-block rounded-3xl my-4 mx-2 p-2">
-              {component}
-            </span>
-          ))}
-        </div>
-      </>
+    const subcontent = (
+      <div className="overflow-x-auto whitespace-nowrap">
+        {charaList.map((component) => (
+          <span className="bg-amber-300 text-black inline-block rounded-3xl m-2 p-2">
+            {component}
+          </span>
+        ))}
+      </div>
     );
+    if (charaList.length === 1) {
+      content = (
+        <>
+          <span className="text-white text-lg">以下の人物で検索しました</span>
+          {subcontent}
+        </>
+      );
+    } else {
+      content = (
+        <>
+          <span className="text-white text-lg">
+            以下の人物で AND 検索しました
+          </span>
+          {subcontent}
+        </>
+      );
+    }
   }
 
-  return <div className="pt-4">{content}</div>;
+  return <div className="my-4 sticky top-[40px] z-20">{content}</div>;
 };
 
 export default SearchCondiList;
