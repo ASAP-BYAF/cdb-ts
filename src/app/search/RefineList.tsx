e57ed4637d11e8000d95a419b25b277a91d2d9f0
teppei <teppei0830.4869@gmail.com>
2024-01-03 17:40:09 +0900

    RefineList.tsx を編集しました。
    絞り込みの人物が１人の時の表示を変更しました。

--- src/app/search/RefineList.tsx
+++ src/app/search/RefineList.tsx
@@ -1,25 +1,40 @@
 const SearchCondiList = (props: { charaList: (string | undefined)[] }) => {
   const { charaList } = props;
-  return (
-    <div className="py-4">
-      {charaList.length === 0 || charaList.includes(undefined) ? (
-        <span className="text-white text-2xl">全話を表示しています</span>
-      ) : (
-        <>
-          <span className="text-white text-2xl">
-            以下の人物で AND 検索しました
-          </span>
-          <div className="overflow-x-auto whitespace-nowrap">
-            {charaList.map((component) => (
-              <span className="bg-amber-300 text-black inline-block rounded-3xl my-4 mx-2 p-2">
-                {component}
-              </span>
-            ))}
-          </div>
-        </>
-      )}
-    </div>
-  );
+
+  let content: JSX.Element = <></>;
+  if (charaList.length === 0 || charaList.includes(undefined)) {
+    content = <span className="text-white text-2xl">全話を表示しています</span>;
+  } else if (charaList.length === 1) {
+    content = (
+      <>
+        <span className="text-white text-2xl">以下の人物で検索しました</span>
+        <div className="overflow-x-auto whitespace-nowrap">
+          {charaList.map((component) => (
+            <span className="bg-amber-300 text-black inline-block rounded-3xl my-4 mx-2 p-2">
+              {component}
+            </span>
+          ))}
+        </div>
+      </>
+    );
+  } else {
+    content = (
+      <>
+        <span className="text-white text-2xl">
+          以下の人物で AND 検索しました
+        </span>
+        <div className="overflow-x-auto whitespace-nowrap">
+          {charaList.map((component) => (
+            <span className="bg-amber-300 text-black inline-block rounded-3xl my-4 mx-2 p-2">
+              {component}
+            </span>
+          ))}
+        </div>
+      </>
+    );
+  }
+
+  return <div className="pt-4">{content}</div>;
 };
 
 export default SearchCondiList;
