090f306a2ecd5d64e2a234b6f541a1b41475aa79
teppei <teppei0830.4869@gmail.com>
2024-01-03 22:25:15 +0900

    HeaderNav.tsx を編集しました。

--- src/components/header/HeaderNav.tsx
+++ src/components/header/HeaderNav.tsx
@@ -4,7 +4,7 @@ const HeaderNav = (): JSX.Element => {
   return (
     <div className="flex sticky top-0 z-20">
       <LinkButton to="/search" plusStyle="flex-1 outline outline-1">
-        登場シーン
+        登場話
       </LinkButton>
       <LinkButton to="/wiseword" plusStyle="flex-1 outline outline-1">
         名言
