79a72285e15bc303a4dff3f2cd861b44af2499fe
teppei <teppei0830.4869@gmail.com>
2024-01-08 18:03:00 +0900

    事件情報をまとめたページの追加しました。
    概要の説明だけが書かれており、これから機能を追加する予定です。

---
+++ src/app/file/File.tsx
@@ -0,0 +1,12 @@
+import FileBase from "app/file/FileBase";
+import BaseFrame from "components/BaseFrame";
+
+const File = () => {
+  return (
+    <BaseFrame>
+      <FileBase />
+    </BaseFrame>
+  );
+};
+
+export default File;
---
+++ src/app/file/FileBase.tsx
@@ -0,0 +1,14 @@
+const FileBase = () => {
+  return (
+    <div className="text-white">
+      <h1 className="text-2xl">
+        各事件の登場人物と重要な出来事、名言をまとめたページ
+      </h1>
+      <p>===================================</p>
+      <p className="text-4xl">~~~ 作成中 ~~~</p>
+      <p>===================================</p>
+    </div>
+  );
+};
+
+export default FileBase;
