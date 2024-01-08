04dfe9415a637476a7c8798c82082bee84f6e587
teppei <teppei0830.4869@gmail.com>
2024-01-08 16:02:09 +0900

    Register.tsx を編集しました。
    グローバルスナックバーのコンテキストを追加しました。

--- src/app/register/Register.tsx
+++ src/app/register/Register.tsx
@@ -2,16 +2,19 @@ import ManagedForm from "./ManagedForm";
 import GlobalSpinnerContextProvider from "contexts/spinner/GlobalSpinnerContext";
 import GlobalModalContextProvider from "contexts/modal/normal/GlobalModalContext";
 import GlobalModalWithInputContextProvider from "contexts/modal/with-input/GlobalModalWithInputContext";
+import GlobalSnackbarContextProvider from "contexts/snackbar/GlobalSnackbarContext";
 
 const Register = (): JSX.Element => {
   return (
-    <GlobalModalWithInputContextProvider>
-      <GlobalModalContextProvider>
-        <GlobalSpinnerContextProvider>
-          <ManagedForm />
-        </GlobalSpinnerContextProvider>
-      </GlobalModalContextProvider>
-    </GlobalModalWithInputContextProvider>
+    <GlobalSnackbarContextProvider>
+      <GlobalModalWithInputContextProvider>
+        <GlobalModalContextProvider>
+          <GlobalSpinnerContextProvider>
+            <ManagedForm />
+          </GlobalSpinnerContextProvider>
+        </GlobalModalContextProvider>
+      </GlobalModalWithInputContextProvider>
+    </GlobalSnackbarContextProvider>
   );
 };
 
